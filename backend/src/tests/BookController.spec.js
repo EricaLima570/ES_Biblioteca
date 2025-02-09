b72353471835ea2e08d28fc34751ef1205f25d5e
Jailson Bezerra <jailson.bezerra_7@hotmail.com>
2025-02-09 17:33:40 -0300

    testes com jest

--- backend/src/tests/BookController.spec.js
+++ backend/src/tests/BookController.spec.js
@@ -1,4 +1,4 @@
-import { createBook, getAllBooks } from '../controllers/bookController.js';
+import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from '../controllers/bookController.js';
 import { createBookService } from '../models/bookModel.js';
 import { validateBook } from '../middlewares/inputValidator.js';
 import * as bookService from "../models/bookModel.js";
@@ -112,6 +112,195 @@ describe('Get All Books', () => {
       status: 200,
       message: 'Livros encontrados com sucesso',
       data: mockBooks,
+    });
+  });
+});
+
+describe('Get Book By Id', () => {
+  it('Deve retornar um livro com status 200', async () => {
+    const mockBook = [
+      {
+        id: 1,
+        title: 'Livro Teste1',
+        author: 'Autor Teste1',
+        price: 130.99,
+        quantity: 3,
+        genre: 'Ficção',
+        description: 'Descrição Teste1',
+        published_year: 2022,
+        image_url: 'http://example.com/livro.jpg'
+      }
+    ];
+
+    jest.spyOn(bookService, 'getBookByIdService').mockResolvedValue(mockBook);
+
+    const req = { params: { id: 1 } };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    await getBookById(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+
+    expect(res.json).toHaveBeenCalledWith({
+      status: 200,
+      message: 'Livro encontrado com sucesso',
+      data: mockBook,
+    });
+  });
+
+  it('Deve retornar 404 se o livro não for encontrado', async () => {
+    jest.spyOn(bookService, 'getBookByIdService').mockResolvedValue(null);
+
+    const req = { params: { id: 1 } };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    await getBookById(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    
+    expect(res.json).toHaveBeenCalledWith({
+      status: 404,
+      message: 'Livro não encontrado',
+      data: null
+    });
+  });
+});
+
+describe('Update Book', () => {
+  it('Deve atualizar um livro com sucesso', async () => {
+    const mockUpdatedBook = {
+      id: 1,
+      title: 'Livro Teste1',
+      author: 'Autor Teste1',
+      price: 130.99,
+      quantity: 3,
+      genre: 'Ficção',
+      description: 'Descrição Teste1',
+      published_year: 2022,
+      image_url: 'http://example.com/livro.jpg'
+    };
+
+    jest.spyOn(bookService, 'updateBookService').mockResolvedValue(mockUpdatedBook);
+
+    const req = { 
+      params: { id: 1 }, 
+      body: { title: 'Novo Título' } 
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    await updateBook(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+
+    expect(res.json).toHaveBeenCalledWith({
+      status: 200,
+      message: "Livro atualizado com sucesso",
+      data: mockUpdatedBook,
+    });
+  });
+
+  it('Deve retornar 404 se o livro não for encontrado', async () => {
+    jest.spyOn(bookService, 'updateBookService').mockResolvedValue(null);
+
+    const req = { 
+      params: { id: 1 }, 
+      body: {} 
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    await updateBook(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+
+    expect(res.json).toHaveBeenCalledWith({
+      status: 404,
+      message: 'Livro não encontrado',
+      data: null
+    });
+  });
+});
+
+describe('Delete Book', () => {
+  it('Deve deletar um livro com sucesso', async () => {
+    const mockDeletedBook  = [
+      {
+        id: 1,
+        title: 'Livro Teste1',
+        author: 'Autor Teste1',
+        price: 130.99,
+        quantity: 3,
+        genre: 'Ficção',
+        description: 'Descrição Teste1',
+        published_year: 2022,
+        image_url: 'http://example.com/livro.jpg'
+      }
+    ];
+
+    jest.spyOn(bookService, 'deleteBookService').mockResolvedValue(mockDeletedBook);
+
+    const req = { params: { id: 1 } };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const next = jest.fn();
+
+    await deleteBook(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      status: 200,
+      message: 'Livro deletado com sucesso',
+      data: mockDeletedBook,
+    });
+  });
+
+  it('Deve retornar 404 se o livro não for encontrado', async () => {
+    jest.spyOn(bookService, 'deleteBookService').mockResolvedValue(null);
+
+    const req = { params: { id: 1 } };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    
+    const next = jest.fn();
+
+    await deleteBook(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+
+    expect(res.json).toHaveBeenCalledWith({
+      status: 404,
+      message: 'Livro não encontrado',
+      data: null
     });
   });
 });
